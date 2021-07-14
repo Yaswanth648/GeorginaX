a7642afa8d2cb13c68ed6fd0e387c4ac59c65bd1
Aaryan <aaryan14032006@gmail.com>
2021-07-14 09:51:18 +0530

    Updated Bot

--- src/main/java/com/example/Bot.java
+++ src/main/java/com/example/Bot.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.example.Helpers.configutation;
+import com.example.Plugins.instagram;
 import com.example.Plugins.Greetings.welcome;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -21,6 +22,7 @@ public class Bot extends TelegramLongPollingBot{
 
     public void sendRequest(Update update, String cmd){
         new welcome().handleRequests(update, cmd);
+        new instagram().handleRequests(update, cmd);
     }
 
     public String getHandler(){
