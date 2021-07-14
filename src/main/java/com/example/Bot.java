1c7425b76c4fa71aa55449cb6ccd217f0e198477
Aaryan <aaryan14032006@gmail.com>
2021-07-14 07:40:32 +0530

    Initial Commit

---
+++ src/main/java/com/example/Bot.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot{
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        
+        
+    }
+
+    @Override
+    public String getBotUsername() {
+        
+        return "";
+    }
+
+    @Override
+    public String getBotToken() {
+        
+        return "";
+    }
+    
+}
