00362601930e363ae79f5dffb58cf9d6e724a92b
Aaryan <aaryan14032006@gmail.com>
2021-07-14 08:08:17 +0530

    changed the feilds to static

--- src/main/java/com/example/Helpers/configutation.java
+++ src/main/java/com/example/Helpers/configutation.java
@@ -4,9 +4,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvBuilder;
 
 public class configutation {
-    private final Dotenv dotenv = new DotenvBuilder().ignoreIfMissing().load();
+    private static final Dotenv dotenv = new DotenvBuilder().ignoreIfMissing().load();
 
-    public String botToken = dotenv.get("botToken");
-    public String botUserName = dotenv.get("botUserName");
-    public String handler = dotenv.get("handler");
+    public static String botToken = dotenv.get("botToken");
+    public static String botUserName = dotenv.get("botUserName");
+    public static String handler = dotenv.get("handler");
 }
