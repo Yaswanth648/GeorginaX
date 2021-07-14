11f7a4374b6ca1546178e5bb7b76aa43085b26c1
Aaryan <aaryan14032006@gmail.com>
2021-07-14 08:35:06 +0530

    added Helper function sendPic

---
+++ src/main/java/com/example/Helpers/sendPic.java
@@ -0,0 +1,26 @@
+package com.example.Helpers;
+
+import java.io.File;
+
+import com.example.Bot;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class sendPic extends Bot {
+
+    public void sendpic(Update update, String caption){
+        InputFile inputFile = new InputFile(new File("src/main/java/com/example/assets/georgina.png"));
+        SendPhoto photo = new SendPhoto(chatId(update), inputFile);
+        photo.setCaption(caption);
+
+        try {
+            execute(photo);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
