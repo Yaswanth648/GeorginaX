166aaf14f193a632f9ca4ed3a14f0c49d81d2116
Aaryan <aaryan14032006@gmail.com>
2021-07-17 20:10:24 +0530

    Fixed & updated covid plug

--- src/main/java/com/example/Plugins/covidData.java
+++ src/main/java/com/example/Plugins/covidData.java
@@ -2,28 +2,30 @@ package com.example.Plugins;
 
 import com.example.Bot;
 import com.example.Master;
+import com.example.Helpers.configutation;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class covidData extends Bot implements Master{
+public class covidData extends Bot implements Master {
 
     @Override
     public void handleRequests(Update update, String cmd) {
-        if(cmd.equalsIgnoreCase(getHandler()+"covid")){
+        if (cmd.equalsIgnoreCase(getHandler() + "covid")) {
             try {
-                HttpResponse<String> response = Unirest.get("https://covid-19-data.p.rapidapi.com/country/all")
-                .header("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
-                .asString();
-                //response.getBody();
+                HttpResponse<String> response = Unirest.get("https://covid-19-data.p.rapidapi.com/country?name=india")
+                        .header("x-rapidapi-key", configutation.rapidXapi)
+                        .header("x-rapidapi-host", "covid-19-data.p.rapidapi.com").asString();
+                // response.getBody();
+                response.getBody();
                 sendMessage(update, response.getBody());
             } catch (UnirestException e) {
                 e.printStackTrace();
             }
         }
-        
+
     }
-    
+
 }
